65b7c1c3a7e5fb2b8d1d8d62f846aaac2ac0a90d
redhandsj <redhandsj@redhands.com>
2016-07-13 08:56:37 +0900

    課題ベースを追加

---
+++ rgsJava2/src/rgsJava2/p13/kadaiAtm/KadaiAtmMain.java
@@ -0,0 +1,21 @@
+/**
+ *
+ */
+package rgsJava2.p13.kadaiAtm;
+
+/**
+ * 課題ATMメインクラス
+ * @author h-dodo
+ *
+ */
+public class KadaiAtmMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+
+	}
+
+}
