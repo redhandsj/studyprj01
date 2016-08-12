c78eda2830877b5350db3ab955413143684032aa
redhandsj <redhandsj@redhands.com>
2016-08-12 13:03:07 +0900

    サンプルを削除

--- notTidyHpInfo/samples/springFrameworkThoroughIntroduction/sample-programs/04-spring-mvc/4.2/firstapp-xml/src/main/webapp/WEB-INF/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-    <h2>Hello World!</h2>
-    <ul>
-        <li><a href="<c:url value='/echo' />">エコーアプリケーションへ</a></li>
-    </ul>
-</body>
-</html>
--- notTidyHpInfo/samples/springFrameworkThoroughIntroduction/sample-programs/14-tutorial/mrs/src/main/java/mrs/domain/repository/room/MeetingRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package mrs.domain.repository.room;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import mrs.domain.model.MeetingRoom;
-
-public interface MeetingRoomRepository extends JpaRepository<MeetingRoom, Integer> {
-}
